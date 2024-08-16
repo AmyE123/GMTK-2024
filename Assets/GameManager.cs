5bbfcbe3ea3cc07faa8b70bc0051329b8c140fb9
AmyE123 <amy.elliott2002@yahoo.co.uk>
2024-08-16 22:53:07 +0100

    Scaling object according to player position

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+using MiniJam160.PostJam;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private Player _player;
+
+    public Player Player => _player;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/ScalableObject.cs
+++ Assets/Scripts/ScalableObject.cs
@@ -1,43 +1,125 @@
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 using UnityEngine;
+using MiniJam160.PostJam;
 
 public class ScalableObject : MonoBehaviour
 {
     [SerializeField] private ScalableObjectProperties properties;
 
-    private float _startingMass;
+    private Player _player;
     private float _currentMass;
     private Rigidbody2D _rigidBody;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    // Initial Values
+    private float _initialMass;
+    private Vector3 _initialScale;
+
+    private void Start()
     {
         _rigidBody = GetComponent<Rigidbody2D>();
-        _startingMass = _rigidBody.mass;
+        _initialMass = _rigidBody.mass;
+        _initialScale = transform.localScale;
+        _player = GameManager.Instance.Player;
+    }
+
+    private void Update()
+    {
+        Vector3 anchorWorldPosition = CalculateClosestEdgeAnchor();
     }
 
     private void OnMouseOver()
     {
+        float initialScale = transform.localScale.x;
+
         if (Input.GetMouseButton(0))
         {
-            float scale = transform.localScale.x;
-            
-            if (scale <= properties.maxScale.x)
+            if (initialScale <= properties.maxScale.x)
             {
-                scale += Time.deltaTime * properties.scaleSpeed;
-                transform.localScale = new Vector3(scale, scale, scale);
-                _currentMass = _startingMass * Mathf.Pow(scale, 3);
+                float newScale = initialScale + Time.deltaTime * properties.scaleSpeed;
+                ScaleObject(newScale);
             }
         }
-        if (Input.GetMouseButton(1))
+        else if (Input.GetMouseButton(1))
         {
-            float scale = transform.localScale.x;
-
-            if(scale >= properties.minScale.x)
+            if (initialScale >= properties.minScale.x)
             {
-                scale -= Time.deltaTime * properties.scaleSpeed;
-                transform.localScale = new Vector3(scale, scale, scale);
-                _currentMass = _startingMass * Mathf.Pow(scale, 3);
+                float newScale = initialScale - Time.deltaTime * properties.scaleSpeed;
+                ScaleObject(newScale);
             }
         }
     }
+
+    private void ScaleObject(float newScale)
+    {
+        Vector3 anchorWorldPositionBeforeScaling = CalculateClosestEdgeAnchor();
+        transform.localScale = new Vector3(newScale, newScale, newScale);
+        Vector3 anchorWorldPositionAfterScaling = CalculateClosestEdgeAnchor();
+        Vector3 positionDelta = anchorWorldPositionBeforeScaling - anchorWorldPositionAfterScaling;
+        _rigidBody.MovePosition(_rigidBody.position + new Vector2(positionDelta.x, positionDelta.y));
+        _currentMass = _initialMass * Mathf.Pow(newScale, 3);
+    }
+
+    private Vector3 CalculateClosestEdgeAnchor()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Bounds bounds = spriteRenderer.bounds;
+        Vector3 playerPosition = _player.transform.position;
+
+        Vector3 topEdge = new Vector3(bounds.center.x, bounds.max.y, bounds.center.z);
+        Vector3 bottomEdge = new Vector3(bounds.center.x, bounds.min.y, bounds.center.z);
+        Vector3 leftEdge = new Vector3(bounds.min.x, bounds.center.y, bounds.center.z);
+        Vector3 rightEdge = new Vector3(bounds.max.x, bounds.center.y, bounds.center.z);
+
+        float distanceToTop = Vector3.Distance(playerPosition, topEdge);
+        float distanceToBottom = Vector3.Distance(playerPosition, bottomEdge);
+        float distanceToLeft = Vector3.Distance(playerPosition, leftEdge);
+        float distanceToRight = Vector3.Distance(playerPosition, rightEdge);
+
+        float minDistance = Mathf.Min(distanceToTop, distanceToBottom, distanceToLeft, distanceToRight);
+
+        if (minDistance == distanceToTop)
+        {
+            return topEdge;
+        }
+        else if (minDistance == distanceToBottom)
+        {
+            return bottomEdge;
+        }
+        else if (minDistance == distanceToLeft)
+        {
+            return leftEdge;
+        }        
+        else
+        {
+            return rightEdge;
+        }            
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Vector3 playerPosition = _player.transform.position;
+
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        Bounds bounds = spriteRenderer.bounds;
+
+        Vector3 topEdge = new Vector3(bounds.center.x, bounds.max.y, bounds.center.z);
+        Vector3 bottomEdge = new Vector3(bounds.center.x, bounds.min.y, bounds.center.z);
+        Vector3 leftEdge = new Vector3(bounds.min.x, bounds.center.y, bounds.center.z);
+        Vector3 rightEdge = new Vector3(bounds.max.x, bounds.center.y, bounds.center.z);
+
+        Handles.color = Color.green;
+        Handles.DrawLine(playerPosition, topEdge);
+        Handles.DrawLine(playerPosition, bottomEdge);
+        Handles.DrawLine(playerPosition, leftEdge);
+        Handles.DrawLine(playerPosition, rightEdge);
+
+        Vector3 anchorWorldPosition = CalculateClosestEdgeAnchor();
+        Handles.color = Color.red;
+        Handles.DrawSolidDisc(anchorWorldPosition, Vector3.forward, 0.1f);
+    }
+#endif
 }
