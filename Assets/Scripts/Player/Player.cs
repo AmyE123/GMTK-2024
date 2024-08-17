70ac2d9d8b162b2b6ef7eb155d18d2d49b3df13a
AmyE123 <amy.elliott2002@yahoo.co.uk>
2024-08-17 13:58:32 +0100

    Cleaned up post jam bits

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(PersonMovement))]
+public class Player : MonoBehaviour
+{
+    private InputSystem_Actions _input;
+    private Vector2 _moveInput;
+    private bool _jumpPressed;
+    PersonMovement _movement;
+
+    void Start()
+    {
+        _movement = GetComponent<PersonMovement>();
+        _input = new();
+        _input.Enable();
+
+        _input.Player.Jump.performed += JumpPressed;
+        _input.Player.Interact.performed += InteractPressed;
+    }
+
+    private void Update()
+    {
+        HandlePlayerInput();
+    }
+
+
+    // Input Event
+    private void JumpPressed(InputAction.CallbackContext ctx)
+    {
+        _jumpPressed = true;
+    }
+
+    // Input Event
+    private void InteractPressed(InputAction.CallbackContext ctx)
+    {
+
+    }
+
+    private void HandlePlayerInput()
+    {
+        _moveInput = _input.Player.Move.ReadValue<Vector2>();
+
+        _movement.SetDesiredMove(_moveInput.x);
+        _movement.SetJumpRequested(_jumpPressed);
+
+        _jumpPressed = false;
+    }
+}
