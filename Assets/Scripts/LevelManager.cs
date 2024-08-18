de3ee305a15d5d2cd394768f2837ce04cd136077
Adam <tingleyadam@gmail.com>
2024-08-18 16:29:50 +0100

    Level manager/loader system

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,35 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity;
 
 public class LevelManager : MonoBehaviour {
 
     [SerializeField] private List<LevelContainer> _levelPrefabs;
 
     private LevelContainer _currentLevel;
+    private int _currentLevelIdx;
 
     void Start() {
         OnNewLevelReached(0);
     }
 
-    private int _currentLevelIdx;
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Equals)) {
+            OnNewLevelReached(_currentLevelIdx + 1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            RespawnSameLevel();
+        }
+    }
 
     public void OnNewLevelReached(int levelNumber) {
         _currentLevelIdx = levelNumber;
 
-        Destroy(_currentLevel.gameObject);
-
+        if (_currentLevel != null) {
+            Destroy(_currentLevel.gameObject);
+        }
 
         int levelNum = Mathf.Clamp(levelNumber, 0, _levelPrefabs.Count - 1);
 
-        _currentLevel = Instantiate(_levelPrefabs[levelNum]);
+        _currentLevel = Instantiate(_levelPrefabs[levelNum], Vector3.zero, Quaternion.identity);
     }
 
     public void RespawnSameLevel() {
         if (_currentLevel != null)
             Destroy(_currentLevel.gameObject);
 
-        _currentLevel = Instantiate(_levelPrefabs[_currentLevelIdx]);
+        _currentLevel = Instantiate(_levelPrefabs[_currentLevelIdx], Vector3.zero, Quaternion.identity);
     }
 }
